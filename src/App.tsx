e48dc90d95f42940cde1caa50843a73f5aa667fe
Hajhosua <josue.gonzalez043@pascualbravo.edu.co>
2026-01-29 20:30:39 -0500

    fix double router github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,29 @@
-import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
+import { Routes, Route, NavLink } from 'react-router-dom';
 import Pines from './pages/Pines';
 import PisaCorbatas from './pages/PisaCorbatas';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="container">
-        <h1 className="titulo">🎵 Mi Tienda Online Musical</h1>
+    <div className="container">
+      <h1 className="titulo">🎵 Mi Tienda Online Musical</h1>
 
-        <nav className="nav">
-          <NavLink to="/pines" className="nav-btn">
-            Pines
-          </NavLink>
+      <nav className="nav">
+        <NavLink to="/pines" className="nav-btn">
+          Pines
+        </NavLink>
 
-          <NavLink to="/pisacorbat" className="nav-btn">
-            Pisa Corbatas
-          </NavLink>
-        </nav>
+        <NavLink to="/pisacorbat" className="nav-btn">
+          Pisa Corbatas
+        </NavLink>
+      </nav>
 
-        <Routes>
-          <Route path="/pines" element={<Pines />} />
-          <Route path="/pisacorbat" element={<PisaCorbatas />} />
-          <Route path="*" element={<h2>Bienvenido</h2>} />
-        </Routes>
-      </div>
-    </Router>
+      <Routes>
+        <Route path="/" element={<h2>Bienvenido</h2>} />
+        <Route path="/pines" element={<Pines />} />
+        <Route path="/pisacorbat" element={<PisaCorbatas />} />
+      </Routes>
+    </div>
   );
 }
 
