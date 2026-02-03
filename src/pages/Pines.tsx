cc2aa4907b6cc58e6c04a82981de97c414d3dc74
Hajhosua <josue.gonzalez043@pascualbravo.edu.co>
2026-02-03 12:52:13 -0500

    Descripción del cambio

--- src/pages/Pines.tsx
+++ src/pages/Pines.tsx
@@ -56,7 +56,7 @@ export default function Pines() {
 
   return (
     <div className="productos">
-      <h2>Pines Disponibles</h2>
+      <h2>Pines Disponible 😁</h2>
 
       <div className="grid-productos">
         {productos.map((prod) => (
